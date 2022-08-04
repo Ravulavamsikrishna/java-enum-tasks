309d1270dffc8417187671916df1ed8e67176cd1
Ravulavamsikrishna <vamshikrishna.r@techouts.com>
2022-08-04 23:24:44 +0530

    java enum examples done

---
+++ java/example1.java
@@ -0,0 +1,15 @@
+package packager.enumm.java;
+
+public  class example1 {
+	static String a[]= {"SUMMER,WINNTER,SPRING,FALL"};
+	
+	public static void main(String[] args) {
+		for(int i=0;i<=a.length;i++) {
+				System.out.println(a[i]);
+		}
+	
+	}
+
+}
+
+
